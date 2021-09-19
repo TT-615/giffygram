915ade547d297be86c74122da830689b7189c8c0
Tia Thomason <88596394+TT-615@users.noreply.github.com>
2021-09-18 20:15:37 -0500

    added PostList/Footer

--- src/scripts/footer/Footer.js
+++ src/scripts/footer/Footer.js
@@ -1,5 +1,14 @@
 export const Footer = () => {
     return `
-    <footer>Established in 2021</footer>
+    <div class="footer__item">
+        Posts since <select id="yearSelection">
+            <option>2020</option>
+            <option>2019</option>
+            <option>2018</option>
+            <option>2017</option>
+        </select>
+        <span id="postCount">0</span>
+    </div>
+
     `
 }
