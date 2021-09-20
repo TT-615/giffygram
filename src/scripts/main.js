14ef73ab99a10a309fca58dc668f6904c55b56d9
Tia Thomason <88596394+TT-615@users.noreply.github.com>
2021-09-20 15:43:43 -0500

    added delete button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 import { getJokes } from "./data/JokesData.js";
-import { getPosts, getUsers, usePostCollection, createPost } from "./data/DataManager.js";
+import { getPosts, getUsers, usePostCollection, createPost, deletePost } from "./data/DataManager.js";
 import { PostList } from "./feed/PostList.js";
 import { PostEntry } from "./feed/PostEntry.js";
 import { NavBar } from "./nav/NavBar.js";
@@ -70,6 +70,7 @@ applicationElement.addEventListener("click", event => {
         })
     }
   })
+
   
 
 getUsers()
@@ -84,7 +85,41 @@ const startGiffyGram = () => {
     showPostList();
     showFooter();
     showPostEntry();
+
     // theJokes();
 }
 
 startGiffyGram();
+
+
+applicationElement.addEventListener("click", event => {
+  if (event.target.id === "newPost__cancel") {
+      //clear the input fields
+  }
+})
+
+applicationElement.addEventListener("click", event => {
+  event.preventDefault();
+  if (event.target.id === "newPost__submit") {
+  //collect the input values into an object to post to the DB
+    const title = document.querySelector("input[name='postTitle']").value
+    const url = document.querySelector("input[name='postURL']").value
+    const description = document.querySelector("textarea[name='postDescription']").value
+    //we have not created a user yet - for now, we will hard code `1`.
+    //we can add the current time as well
+    const postObject = {
+        title: title,
+        imageURL: url,
+        description: description,
+        userId: 1,
+        timestamp: Date.now()
+    }
+
+  // be sure to import from the DataManager
+      createPost(postObject)
+      .then(dbResponse => {
+        showPostList();
+        showPostEntry();
+      })
+  }
+})
